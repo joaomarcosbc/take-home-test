0c6f61c7e3e10b3af57e7abbef1933ed611df37c
João Marcos <joaombcerqueira@gmail.com>
2025-10-20 12:27:25 -0300

    feat: adds repositories contracts

---
+++ backend/src/Fundo.Applications.Application/Repositories/ILoanPaymentRepository.cs
@@ -0,0 +1,16 @@
+﻿using Fundo.Applications.Domain.Entities;
+
+namespace Fundo.Applications.Application.Repositories;
+
+/// <summary>
+/// Defines methods for managing <see cref="LoanPayment"/> entities in the data store.
+/// </summary>
+public interface ILoanPaymentRepository
+{
+    /// <summary>
+    /// Creates a new loan payment.
+    /// </summary>
+    /// <param name="loanPayment">The <see cref="LoanPayment"/> entity to create.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    Task CreateAsync(LoanPayment loanPayment);
+}
---
+++ backend/src/Fundo.Applications.Application/Repositories/ILoanRepository.cs
@@ -0,0 +1,38 @@
+﻿using Fundo.Applications.Domain.Entities;
+
+namespace Fundo.Applications.Application.Repositories;
+
+/// <summary>
+/// Defines methods for managing <see cref="Loan"/> entities in the data store.
+/// </summary>
+public interface ILoanRepository
+{
+    /// <summary>
+    /// Creates a new loan.
+    /// </summary>
+    /// <param name="loan">The <see cref="Loan"/> entity to create.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    Task CreateAsync(Loan loan);
+
+    /// <summary>
+    /// Updates an existing loan.
+    /// </summary>
+    /// <param name="loan">The <see cref="Loan"/> entity to update.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    Task UpdateAsync(Loan loan);
+
+    /// <summary>
+    /// Retrieves a paginated list of loans.
+    /// </summary>
+    /// <param name="pageNumber">The page number (default is 1).</param>
+    /// <param name="pageSize">The number of items per page (default is 10).</param>
+    /// <returns>A task that returns a read-only list of <see cref="Loan"/> entities.</returns>
+    Task<IReadOnlyList<Loan>> GetAllAsync(int pageNumber = 1, int pageSize = 10);
+
+    /// <summary>
+    /// Retrieves a loan by its ID.
+    /// </summary>
+    /// <param name="id">The unique identifier of the loan.</param>
+    /// <returns>A task that returns the <see cref="Loan"/> if found; otherwise, <c>null</c>.</returns>
+    Task<Loan?> GetByIdAsync(int id);
+}
