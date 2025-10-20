9691f4399483c07fe2a9d3eca56b97402d4690b4
João Marcos <joaombcerqueira@gmail.com>
2025-10-20 12:24:44 -0300

    feat: implements exception handling behavior

---
+++ backend/src/Fundo.Applications.Application/Behaviors/ExceptionHandlingBehavior.cs
@@ -0,0 +1,26 @@
+﻿using FluentResults;
+using Fundo.Applications.Packages.ResultsSerialization.Errors;
+using MediatR;
+
+namespace Fundo.Applications.Application.Behaviors;
+
+public class ExceptionHandlingBehavior<TRequest, TResponse>
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        try
+        {
+            return await next();
+        }
+        catch (Exception ex)
+        {
+            var error = new InternalServerError(ex.Message, ex);
+            return (TResponse)(object)Result.Fail(error);
+        }
+    }
+}
