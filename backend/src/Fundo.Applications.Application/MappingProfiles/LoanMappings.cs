3b550e9c5d736d361d538cfe16a8f7a9ce6b024f
João Marcos <joaombcerqueira@gmail.com>
2025-10-20 12:37:36 -0300

    feat: implements loan mapping profile

---
+++ backend/src/Fundo.Applications.Application/MappingProfiles/LoanMappings.cs
@@ -0,0 +1,54 @@
+﻿using Fundo.Applications.Application.UseCases.Loans.CreateLoan;
+using Fundo.Applications.Application.UseCases.Loans.GetAllLoans;
+using Fundo.Applications.Application.UseCases.Loans.GetLoan;
+using Fundo.Applications.Domain.Entities;
+using Fundo.Applications.Domain.Enums;
+
+namespace Fundo.Applications.Application.MappingProfiles;
+
+public static class LoanMappings
+{
+    public static Loan ToEntity(this CreateLoanRequest request)
+    {
+        return new Loan
+        {
+            Amount = request.Amount,
+            CurrentBalance = request.CurrentBalance ?? 0m,
+            ApplicantName = request.ApplicantName
+        };
+    }
+
+    public static GetAllLoansResponse ToGetAllLoansResponse(this Loan model)
+    {
+        return new GetAllLoansResponse(
+            model.Id,
+            model.Amount,
+            model.CurrentBalance,
+            model.ApplicantName,
+            model.Status);
+    }
+
+    public static IEnumerable<GetAllLoansResponse> ToGetAllLoansResponse(this IEnumerable<Loan> models)
+    {
+        return models.Select(m => m.ToGetAllLoansResponse());
+    }
+
+    public static GetLoanResponse ToGetLoanResponse(this Loan loan)
+    {
+        var payments = loan.Payments?.Select(p => new GetLoanPaymentResponse(
+            p.Amount,
+            p.DateCreated
+        )) ?? Enumerable.Empty<GetLoanPaymentResponse>();
+
+        return new GetLoanResponse(
+            loan.Amount,
+            loan.CurrentBalance,
+            loan.ApplicantName,
+            loan.Status,
+            loan.Amount - loan.CurrentBalance,
+            loan.DateCreated,
+            loan.DateModified,
+            payments
+        );
+    }
+}
