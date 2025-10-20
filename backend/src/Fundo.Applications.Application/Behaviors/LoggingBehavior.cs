c6b24dbc8878c11c7b249c76e0a2c6bf35d55a43
João Marcos <joaombcerqueira@gmail.com>
2025-10-20 12:25:32 -0300

    feat: implements logging behavior

---
+++ backend/src/Fundo.Applications.Application/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,35 @@
+﻿using FluentResults;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Fundo.Applications.Application.Behaviors;
+
+public sealed class LoggingBehavior<TRequest, TResponse>
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : notnull
+{
+    private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger;
+
+    public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        var response = await next();
+
+        if (response is Result result && result.IsFailed)
+        {
+            foreach (var error in result.Errors)
+            {
+                _logger.LogError("Request {RequestName} failed: {Error}", typeof(TRequest).Name, error.Message);
+            }
+        }
+
+        return response;
+    }
+}
