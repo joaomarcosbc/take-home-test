0c60c864cd1127378bc81858b5634917ed1236d3
João Marcos <joaombcerqueira@gmail.com>
2025-10-20 21:38:16 -0300

    feat: adds response models

---
+++ frontend/src/app/models/loan.model.ts
@@ -0,0 +1,7 @@
+export interface Loan {
+  id: number;
+  amount: number;
+  currentBalance: number;
+  applicantName: string;
+  status: number;
+}
---
+++ frontend/src/app/models/paginates-response.model.ts
@@ -0,0 +1,6 @@
+export interface PaginatedResponse<T> {
+  items: T[];
+  totalCount: number;
+  pageNumber: number;
+  pageSize: number;
+}
